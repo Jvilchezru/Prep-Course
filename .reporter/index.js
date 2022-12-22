0822f13864a08983d9c595bfe6ad79f90bc39333
Jvilchezru <vilchez.10.2001@gmail.com>
2022-12-22 10:54:40 -0500

    JS-I-Homework

--- .reporter/index.js
+++ .reporter/index.js
@@ -27,29 +27,29 @@ function searchEmail(str) {
   return re.exec(str);
 }
 function getEmail(PATH) {
-	try {
-		const email = searchEmail(fs
-			.readFileSync(PATH)
+  try {
+    const email = searchEmail(fs
+      .readFileSync(PATH)
       .toString());
-		if (email.length > 0) {
-			return email[0];
-		} else {
-			console.log(`
+    if (email.length > 0) {
+      return email[0];
+    } else {
+      console.log(`
             configura tu cuenta de git por favor!
             https://git-scm.com/book/es/v2/Personalizaci%C3%B3n-de-Git-Configuraci%C3%B3n-de-Git`);
-			process.exit()
-		}
-	} catch (error) {
-		console.log(
-			`Tienes que instalar git!
+      process.exit()
+    }
+  } catch (error) {
+    console.log(
+      `Tienes que instalar git!
         https://git-scm.com/book/es/v2/Inicio---Sobre-el-Control-de-Versiones-Instalaci%C3%B3n-de-Git`
-		);
-		process.exit()
-	}
+    );
+    process.exit()
+  }
 }
 
 function readFile() {
-  try{
+  try {
     const lt = fs.readFileSync('./.reporter/lt.json');
     return JSON.parse(lt);
   } catch (e) {
@@ -63,45 +63,45 @@ module.exports = function report(data) {
     const lastRun = readFile();
     const username = getGitEmail();
     gitRemoteOriginUrl()
-        .then((remote) => {
-          const { name: repo, owner: github } = GitUrlParse(remote);
-          const pedidos = data.testResults.map(test => {
-            if(!lastRun[test.testFilePath]) {
-              lastRun[test.testFilePath] = {
-                passing: test.numPassingTests,
-                tries: 0,
-              }
+      .then((remote) => {
+        const { name: repo, owner: github } = GitUrlParse(remote);
+        const pedidos = data.testResults.map(test => {
+          if (!lastRun[test.testFilePath]) {
+            lastRun[test.testFilePath] = {
+              passing: test.numPassingTests,
+              tries: 0,
             }
-            if(lastRun[test.testFilePath].passing != test.numPassingTests || lastRun[test.testFilePath].tries == 0) {
-              const tries = lastRun[test.testFilePath].tries;
-              lastRun[test.testFilePath].passing = test.numPassingTests;
-              lastRun[test.testFilePath].tries = 1;
-              return axios.post('https://proxy.soyhenry.com:3001/m0/grade', {
-                "pending": test.numPendingTests,
-                "passing": test.numPassingTests,
-                "failed": test.numFailingTests,
-                "runtime": test.perfStats.runtime,
-                "slow": test.perfStats.slow,
-                "file": path.basename(test.testFilePath),
-                "repo": repo,
-                "github": github,
-                "username": username,
-                "tries": tries
-              });
-          } else {
-              lastRun[test.testFilePath].tries++;
-              lastRun[test.testFilePath].passing = test.numPassingTests;
-              return Promise.resolve();
           }
-          });
-          return Promise.all(pedidos);
-        })
-        .then((results) => {
-          fs.writeFileSync('./.reporter/lt.json', JSON.stringify(lastRun));
-        })
-        .catch((err) => {
-          console.log(err);
+          if (lastRun[test.testFilePath].passing != test.numPassingTests || lastRun[test.testFilePath].tries == 0) {
+            const tries = lastRun[test.testFilePath].tries;
+            lastRun[test.testFilePath].passing = test.numPassingTests;
+            lastRun[test.testFilePath].tries = 1;
+            return axios.post('https://proxy.soyhenry.com:3001/m0/grade', {
+              "pending": test.numPendingTests,
+              "passing": test.numPassingTests,
+              "failed": test.numFailingTests,
+              "runtime": test.perfStats.runtime,
+              "slow": test.perfStats.slow,
+              "file": path.basename(test.testFilePath),
+              "repo": repo,
+              "github": github,
+              "username": username,
+              "tries": tries
+            });
+          } else {
+            lastRun[test.testFilePath].tries++;
+            lastRun[test.testFilePath].passing = test.numPassingTests;
+            return Promise.resolve();
+          }
         });
+        return Promise.all(pedidos);
+      })
+      .then((results) => {
+        fs.writeFileSync('./.reporter/lt.json', JSON.stringify(lastRun));
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   } catch (error) {
     console.error(error);
   }
